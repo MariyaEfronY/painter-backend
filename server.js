b7027c8f548c1924b458fb9503a3518da5c20fe2
MariyaEfronY <mariyaefrony@gmail.com>
2025-09-01 23:00:54 +0530

    secont

--- server.js
+++ server.js
@@ -84,4 +84,5 @@ mongoose.connect(process.env.MONGO_URI, {
 // ✅ Start server
 // const PORT = process.env.PORT || 5000;
 // app.listen(PORT, () => {
-//   console.log(`🚀 Server running at http://localhost:${PORT}`
+//   console.log(`🚀 Server running at http://localhost:${PORT}`);
+// });
